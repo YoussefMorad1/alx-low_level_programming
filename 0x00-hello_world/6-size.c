7b0d126fc5ea81b47edd567d9ba7c0e68353a138
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-16 13:51:14 -0800

    fullpush

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,21 +11,21 @@ int main(void)
 	long y;
 	long long z;
 	float f;
-	
+
 	printf("Size of a char: ");
 	printf("%d", sizeof(ch));
 	printf(" byte(s)\n");
 	printf("Size of an int: ");
-        printf("%d", sizeof(x));
-        printf(" byte(s)\n");
+	printf("%d", sizeof(x));
+	printf(" byte(s)\n");
 	printf("Size of a long int: ");
-        printf("%d", sizeof(y));
-        printf(" byte(s)\n");
+	printf("%d", sizeof(y));
+	printf(" byte(s)\n");
 	printf("Size of a long long int: ");
-        printf("%d", sizeof(z));
-        printf(" byte(s)\n");
+	printf("%d", sizeof(z));
+	printf(" byte(s)\n");
 	printf("Size of a float: ");
-        printf("%d", sizeof(f));
-        printf(" byte(s)\n");
+	printf("%d", sizeof(f));
+	printf(" byte(s)\n");
 	return (0);
 }
