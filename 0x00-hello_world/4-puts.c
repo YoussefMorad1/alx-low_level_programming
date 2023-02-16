61b39e6e78d1923794d695d6cb93cf5479454052
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-16 10:12:38 -0800

    FullPush

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * main - our main function
- *
+ * 
  * Description: prints sentence
  * Return: returns 0
  */
 int main(void)
 {
-	char* s = "\"Programming is like building a multilingual puzzle";
+	char *s = "\"Programming is like building a multilingual puzzle";
+
 	puts(s);
 	return (0);
 }
