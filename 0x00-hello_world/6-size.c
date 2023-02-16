39a8eb7872b20d41d6957746406746561ce057ac
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-16 13:46:30 -0800

    fullpush

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
- * description - hi
- * return - 0
+ * main - our main function
+ * Description: prints sentence
+ * Return: returns 0
  */
-
 int main(void)
 {
 	char ch;
@@ -14,23 +13,19 @@ int main(void)
 	float f;
 	
 	printf("Size of a char: ");
-	printf("%d", sizeof ch);
+	printf("%d", sizeof(ch));
 	printf(" byte(s)\n");
-
 	printf("Size of an int: ");
-        printf("%d", sizeof x);
+        printf("%d", sizeof(x));
         printf(" byte(s)\n");
-
 	printf("Size of a long int: ");
-        printf("%d", sizeof y);
+        printf("%d", sizeof(y));
         printf(" byte(s)\n");
-
 	printf("Size of a long long int: ");
-        printf("%d", sizeof z);
+        printf("%d", sizeof(z));
         printf(" byte(s)\n");
-
 	printf("Size of a float: ");
-        printf("%d", sizeof f);
+        printf("%d", sizeof(f));
         printf(" byte(s)\n");
 	return (0);
 }
