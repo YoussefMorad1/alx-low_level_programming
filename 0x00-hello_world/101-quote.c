6f29c79938f3d163d70e2aa77c27f9bc575ccc1c
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-16 14:15:05 -0800

    fullpush

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - our main function
  * Description: prints a long sentenc
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	fwrite(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59)
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59)
 	return (0);
 }
