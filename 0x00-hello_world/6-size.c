7e334b7b9fe89aa4d7a444b6aeb5f2a478ef693d
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-16 13:42:22 -0800

    fullpush

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * description - hi
+ * return - 0
+ */
+
+int main(void)
+{
+	char ch;
+	int x;
+	long y;
+	long long z;
+	float f;
+	
+	printf("Size of a char: ");
+	printf("%d", sizeof ch);
+	printf(" byte(s)\n");
+
+	printf("Size of an int: ");
+        printf("%d", sizeof x);
+        printf(" byte(s)\n");
+
+	printf("Size of a long int: ");
+        printf("%d", sizeof y);
+        printf(" byte(s)\n");
+
+	printf("Size of a long long int: ");
+        printf("%d", sizeof z);
+        printf(" byte(s)\n");
+
+	printf("Size of a float: ");
+        printf("%d", sizeof f);
+        printf(" byte(s)\n");
+	return (0);
+}
