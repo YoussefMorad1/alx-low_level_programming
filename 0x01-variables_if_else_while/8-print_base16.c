0c7531a8e4524fdd41d158392fc753d87b619df5
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-17 17:03:08 -0800

    fullpush

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 15; i++){
+		if(i <= 9)
+			putchar('0' + i);
+		else
+			putchar('A' + i - 10);
+	}
+	putchar('\n');
+	return (0);
+}
