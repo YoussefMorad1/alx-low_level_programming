c5dfd7e8cad315bd98912b70a827e24756113dec
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-17 17:48:05 -0800

    fullpush

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(' ');
+			for (k = i; k <= 9; k++)
+			{
+				for (l = j + 1; l <= 9; l++)
+				{
+					putchar('0' + k);
+					putchar('0' + l);
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
