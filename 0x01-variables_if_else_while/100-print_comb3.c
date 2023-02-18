8b839e8fa1ec0050da98d6d3592966e2ab9e3bd0
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-17 17:36:25 -0800

    fullpush

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (; i <= 9; i++)
 	{
-		for(j = i+1; j <= 9; j++) 
+		for (j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
 			putchar('0' + j);
-			if (i != 8 || j != 9) 
+			if (i != 8 || j != 9)
 			{
 				putchar(',');
 				putchar(' ');
