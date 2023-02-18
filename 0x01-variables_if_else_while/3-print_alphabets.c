764f075f191ef6bbb3d1ccb1842c98eec0af4a86
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-17 16:39:57 -0800

    fullpush

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	for (; i <= 'z'; i++)
+		putchar(i);
+	for (; j <= 'Z'; j++)
+		putchar(z);
+	putchar('\n');
+	return (0);
+}
