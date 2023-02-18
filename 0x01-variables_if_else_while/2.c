de4cc9668894bda2752a98119269696b93a2dc00
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-17 16:09:20 -0800

    fullpush

---
+++ 0x01-variables_if_else_while/2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+int main(void)
+{
+	char i = 'a';
+
+	for (; i <= 'z'; i++){
+	    putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
