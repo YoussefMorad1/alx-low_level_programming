3a019409b4d79d61dd9a9456908a86e9c048c37c
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 15:11:07 -0800

    fullpush

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,5 +11,5 @@
 int print_last_digit(int n)
 {
 	_putchar('0' + abs(n) % 10);
-	return (n % 10);
+	return (abs(n) % 10);
 }
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - hi hi
+ * Description: samerr
+ * Return: bye bye
+ */
+void times_table(void)
+{
+	int i, j, ct;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = i, ct = 0; ct < 10; j += i, ct++)
+		{
+			if (j < 10)
+			{
+				_putchar('0' + j);
+			}
+			else
+			{
+				_puthchar('0' + j / 10);
+				_puthchar('0' + j % 10);
+			}
+			_putchar(',');
+			if (ct != 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('$');
+				_putchar('\n');
+			}
+		}
+	}
+}
