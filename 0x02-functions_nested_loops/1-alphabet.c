2eb0e86c36bfa1d0bb07d73c6ab39f1b49879b7d
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 13:36:15 -0800

    fullpush

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (;i <= 'z'; ++i)
+		_putchar(i);
+	_putchar('\n');
+	return;
+}
