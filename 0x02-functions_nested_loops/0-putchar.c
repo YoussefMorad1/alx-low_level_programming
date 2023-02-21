a80f8d549df1c7e360dd788b7824ad53d4fcefbd
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 13:18:32 -0800

    fullpush

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main func
+ * Description: hi hi hi
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return 0;
+}
