2aec880b462c6ac960b03d9ddf70fd4274938c4e
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 15:17:44 -0800

    fullpush

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - hi hi
+ * Description: samerr
+ * @a: sh
+ * @b: shs
+ * Return: bye bye
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
