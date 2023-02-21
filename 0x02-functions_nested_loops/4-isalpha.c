8b532e1e4132c6bd0b57acea97793c5388d51349
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 13:49:29 -0800

    fullpush

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,13 +4,13 @@
 #include <string.h>
 #include "main.h"
 /**
- * print_alphabet_x10 - main func
+ * _isalpha - main func
  * Description: hi hi hi
  * Return: 0
  */
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z' || _islower(c))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 		return (1);
 	return (0);
 }
