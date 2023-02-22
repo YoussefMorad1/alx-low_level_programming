bd15dbfad52802b90cc889092ef86d06547d3c12
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-21 16:09:23 -0800

    fullpush

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * ln - hi hi
- * Description: samerr
- * @x: sh
- * Return: bye bye
- */
-int ln(int x)
-{
-	int i = 1;
-	int	j;
-	int ct = 0;
-	
-	if (!x)
-		return (1);
-	for (j = 1; j <= i; j *= 10)
-	{
-		ct++;
-	}
-	return (ct);
-}
-/**
  * print_to_98 - hi hi
  * Description: samerr
  * @n: sh
@@ -33,35 +13,15 @@ void print_to_98(int n)
 	{
 		for (; n < 98; ++n)
 		{
-			int x = abs(n), l = ln(abs(n));
-
-			if (n < 0)
-				_putchar('-');
-			while (l)
-			{
-				_putchar('0' + (x / l) % 10);
-				l /= 10;
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
 	else
 	{
 		for (; n > 98; --n)
 		{
-			int x = n, l = ln(n);
-
-			while (l)
-			{
-				_putchar('0' + (x / l) % 10);
-				l /= 10;
-			}
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
+	printf("98\n");
 }
