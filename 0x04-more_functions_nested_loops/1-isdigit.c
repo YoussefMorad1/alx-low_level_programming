66167ce7c7da4ee449b869de7361275ff8065088
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-23 13:40:25 -0800

    aa

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - zz
+ * Description: hi bye
+ * @c: int c
+ * Return: 0
+ */
+int _isupper(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
