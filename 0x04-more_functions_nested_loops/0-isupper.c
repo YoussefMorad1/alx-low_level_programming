8f93cfb99430229d3548ba7e92bd56ff791cda4b
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-23 13:34:02 -0800

    aa

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _ispupper - zz
+ * Description: hi bye
+ * @c: int c
+ * Return: 0
+	 */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
