db08c489b7743d5ea62fd7ba96a54db358cbcada
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-23 16:06:54 -0800

    aa

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,11 @@ void more_numbers(void)
 		int i = 0;
 
 		for (; i <= 14; ++i)
-			if (i < 10)
-				_putchar(i + '0');
-			else
-			{
-				_putchar(i/10 + '0');
-				_puthcar(i%10 + '0');
-			}
+		{
+			if (i >= 10)
+				_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+		}
 		_putchar('\n');
 	}
 }
