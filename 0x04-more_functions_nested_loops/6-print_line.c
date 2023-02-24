d90ab693bb1ca1c04060a9b4c5393d82842eeebe
Youssef Morad <youssefmohammed747@gmail.com>
2023-02-23 16:16:00 -0800

    aa

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - hi
+ * @n: s
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+		_putchar('_');
+	_putchar('\n');
+}
