d8ce04362cea23fa19a6c26dada9fe57e809a76f
Youssef <youssefmohammed747@gmail.com>
2023-02-28 01:24:22 +0200

    aa

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	for (; i == 0 || (str[i] != '\0' && s[i-1] != '\0'); i += 2)
+	for (; i == 0 || (str[i] != '\0' && str[i-1] != '\0'); i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
