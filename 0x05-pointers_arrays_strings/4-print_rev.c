e5708daab469c9f610f4046bf781d67b98774628
Youssef <youssefmohammed747@gmail.com>
2023-02-28 01:03:37 +0200

    aa

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - hi
+ * @str : hi
+ * Return : hi
+*/
+void print_rev(char *str)
+{
+	int i = 0, j;
+
+	for (; str[i] != '\0'; ++i)
+		;
+
+	j = i-1;
+	for (; j >= 0; --j)
+		_putchar(s[j]);
+
+	_putchar('\n');
+}
