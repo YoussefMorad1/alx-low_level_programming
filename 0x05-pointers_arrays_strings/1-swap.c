948fb5fe068a09b146a10e2e7f61195d3189d297
Youssef <youssefmohammed747@gmail.com>
2023-02-28 00:41:53 +0200

    aa

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - hi
+ * Description - ss
+ * @a : p
+ * @b : c
+ * Return: hi
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
