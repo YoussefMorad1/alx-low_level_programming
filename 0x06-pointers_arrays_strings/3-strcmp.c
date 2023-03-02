291d4aadb662100ab88c78ccd71bc546d70cec92
Youssef <youssefmohammed747@gmail.com>
2023-03-02 03:50:28 +0200

    hi

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcmp - hi
+ * @s : hi
+ * @ss : yo
+ * Return: hi
+*/
+char *_strcmp(char *s, char *ss)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0' && ss[i] != '\0'; ++i)
+		if (s[i] < ss[i]) 
+			return -1;
+		else if (s[i] > ss[i]) 
+			return 1;
+	if (s[i] == ss[i])
+		return 0;
+	else if (s[i] == '\0')
+		return -1;
+	else
+		return 1;
+	return (0);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *s, char *ss, int n);
 char *_strncpy(char *s, char *ss, int n);
+int _strcmp(char *s1, char *s2);
