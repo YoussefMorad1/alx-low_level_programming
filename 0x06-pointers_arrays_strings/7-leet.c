7dd6bd65f31b50bd9009c82bfc3f974aedf8c934
Youssef <youssefmohammed747@gmail.com>
2023-03-02 20:15:14 +0200

    hi

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,22 @@
 char *leet(char *s)
 {
 	char i = 'A';
-//	char arr[200];
+	char arr[300];
 	int j = 0;
 
-//	for (; i <= 'Z'; i++)
-//		arr[i] = i;
-//	for (i = 'a'; i <= 'z'; i++)
-//		arr[i] = i;
+	for (; i <= 'z'; i++)
+		arr[i] = i;
 
-//	arr['a'] = arr['A'] = '4';
-//	arr['E'] = arr['e'] = '3';
-//	arr['T'] = arr['t'] = '7';
-//	arr['O'] = arr['o'] = '0';
-//	arr['L'] = arr['l'] = '1';
+	arr['a'] = arr['A'] = '4';
+	arr['E'] = arr['e'] = '3';
+	arr['T'] = arr['t'] = '7';
+	arr['O'] = arr['o'] = '0';
+	arr['L'] = arr['l'] = '1';
 
 	for (; s[j] != '\0'; j++)
 	{
 		if ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'));
-//			s[j] = arr[s[j]];
+			s[j] = arr[s[j]];
 	}
 	return (s);
 }
