54b4e52f224de8689f9b076e59c1a099f55fb1ea
Youssef <youssefmohammed747@gmail.com>
2023-03-02 04:14:43 +0200

    hi

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * reverse_array - hi
- * @s : hi
- * @ss : yo
+ * @a : hi
+ * @n : yo
  * Return: hi
 */
 void reverse_array(int *a, int n)
 {
 	int i = 0;
-	
+
 	for (; i < n / 2; ++i)
 	{
 		int tmp = a[n - i - 1];
+
 		a[n - i - 1] = a[i];
 		a[i] = tmp;
 	}
