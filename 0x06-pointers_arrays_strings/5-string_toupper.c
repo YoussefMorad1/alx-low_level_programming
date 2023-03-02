f77d484e3505f9ccc20be0b4f00341467d4f480d
Youssef <youssefmohammed747@gmail.com>
2023-03-02 04:19:39 +0200

    hi

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strncpy - hi
+ * @s : hi
+ * Return: hi
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; ++i)
+		s[i] -= (s[i] >= 'a'? 32 : 0);
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,3 +4,6 @@ char *_strncat(char *s, char *ss, int n);
 char *_strncpy(char *s, char *ss, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
