09f8f13ee9fdaf29f86383e7d17b0b1e9217142f
Youssef <youssefmohammed747@gmail.com>
2023-03-02 04:11:09 +0200

    hi

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,3 +3,4 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *s, char *ss, int n);
 char *_strncpy(char *s, char *ss, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
