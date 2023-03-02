9dbe9e2560af7c606c68ee180b8359428e40e352
Youssef <youssefmohammed747@gmail.com>
2023-03-02 03:54:24 +0200

    hi

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,11 @@ int _strcmp(char *s, char *ss)
 	int i = 0;
 
 	for (i = 0; s[i] != '\0' && ss[i] != '\0'; ++i)
-		if (s[i] < ss[i]) 
+		if (s[i] < ss[i])
 			return (-1);
 		else if (s[i] > ss[i])
 			return (1);
-	if (s[i] == ss[i])
+	if (s[i] == '\0' && ss[i] == '\0')
 		return (0);
 	else if (s[i] == '\0')
 		return (-1);
