3c52996ae386ba98ab806511e911afccb2f13aab
Youssef <youssefmohammed747@gmail.com>
2023-03-02 03:45:00 +0200

    hi

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,7 +10,7 @@ char *_strncpy(char *s, char *ss, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n && ss[i] != '\0'; ++j)
+	for (i = 0; i < n && ss[i] != '\0'; ++i)
 		s[i] = ss[i];
 	for (; i < n; ++i)
 		s[i] = '\0';
