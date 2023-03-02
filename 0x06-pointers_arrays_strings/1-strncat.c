17bcdb95b838b71b3dcc95f2c41ca76cebbcf4d1
Youssef <youssefmohammed747@gmail.com>
2023-03-02 03:40:23 +0200

    hi

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,7 @@ char *_strncat(char *s, char *ss, int n)
 
 	while (s[i] != '\0')
 		++i;
-	for (j = i; j - i < n && ss[j = i] != '\0'; ++j)
+	for (j = i; j - i < n && ss[j - i] != '\0'; ++j)
 		s[j] = ss[j - i];
 	s[j] = '\0';
 	return (s);
