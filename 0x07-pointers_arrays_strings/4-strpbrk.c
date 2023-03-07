73405d3586135bfc2ff21c77c08638c2f0f3c31e
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-07 22:52:13 +0200

    aa

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdbool.h>
+/**
+ * _strpbrk - hi
+ * @s : hi
+ * @a : yo
+ * Return: hi
+*/
+char *_strpbrk(char *s, char *a)
+{
+	int i = 0, j = 0;
+	bool found;
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[i])
+				return (s + i);
+		}
+	}
+	return (0);
+}
