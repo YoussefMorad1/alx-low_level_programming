c7da11d7fd17a3ed2057be835b34aa69ed45bb9a
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-07 22:01:44 +0200

    aa

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,14 +7,15 @@
 */
 char *_strchr(char *s, char c)
 {
-	unsigned int i = -1;
+	unsigned int i = 0;
 
-	while (i++)
+	while (1)
 	{
 		if (s[i] == c)
 			return (s + i);
 		else if (s[i] == '\0')
 			break;
+		i++;
 	}
 	return ((void *)0);
 }
