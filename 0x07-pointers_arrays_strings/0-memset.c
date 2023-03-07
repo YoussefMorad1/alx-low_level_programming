bec04f0787ee67c7d837094963f0c206d185e897
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-06 17:17:43 -0800

    hh

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - hi
+ * @s : hi
+ * @b : yo
+ * @n : yo
+ * Return: hi
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
