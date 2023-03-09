7bb96bd42a8b02101fbe5e749f9d0582d25279cd
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-09 17:02:55 +0200

    fullpush

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,7 +7,7 @@
  */
 int p(int n, int i)
 {
-	if (i * i > n)	
+	if (i * i > n)
 		return (1);
 	if (n % i == 0)
 		return (0);
@@ -20,7 +20,7 @@ int p(int n, int i)
 */
 int is_prime_number(int n)
 {
-	if (n <= 1) 
+	if (n <= 1)
 		return (0);
 	return (p(n, 2));
 }
