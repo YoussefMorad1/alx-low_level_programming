53a1a32c28ba40e2afa58eedf1aeb8539ac6fba7
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-09 16:53:18 +0200

    fullpush

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
  * is_prime_number - hi
- * @x : yo
- * @y : ho
+ * @n : yo
  * Return: hi
 */
 int is_prime_number(int n)
 {
-	for (int i = 2; i*i <= n; i++)
+	int i;
+
+	for (i = 2; i * i <= n; i++)
 	{
 		if (n % i == 0)
 			return (0);
-	}	
+	}
 	return (1);
 }
