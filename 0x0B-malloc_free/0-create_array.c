b2710af42aabea78421cb5b39eddf06265711255
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-15 01:23:06 +0200

    fullpush

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,19 +1,20 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * create_arry - hi
+ * create_array - hi
  * @s : yo
  * @c : yo
  * Return: hi
 */
 char *create_array(unsigned int s, char c)
 {
-	char* ptr;
+	char *ptr;
 	int i = 0;
 
 	if (s == 0)
 		return (0);
 	ptr = malloc(s * sizeof(char));
-	while(i < s)
+	while (i < s)
 	{
 		ptr[i++] = c;
 	}
