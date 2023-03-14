2d8de8ec77157b51f097eb2defd975923f95117d
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-15 01:22:06 +0200

    fullpush

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_arry - hi
+ * @s : yo
+ * @c : yo
+ * Return: hi
+*/
+char *create_array(unsigned int s, char c)
+{
+	char* ptr;
+	int i = 0;
+
+	if (s == 0)
+		return (0);
+	ptr = malloc(s * sizeof(char));
+	while(i < s)
+	{
+		ptr[i++] = c;
+	}
+	return (ptr);
+}
