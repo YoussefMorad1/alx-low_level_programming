e60814e051824652584c679c9c09b533360d68a5
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-15 01:31:40 +0200

    fullpush

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ _strdup  - hi
+ * @s : yo
+ * Return: hi
+*/
+char *_strdup(char *s)
+{
+	char *ptr;
+	unsigned int i = 0, j = 0;
+	
+	if (!s || !s[j]) 
+		return (0);
+	while (s[j])
+		j++;
+	j++;
+	ptr = malloc(j * sizeof(char));
+	if (!ptr)
+		return (0);
+	while (i < j)
+	{
+		ptr[i] = s[i++];
+	}
+	return (ptr);
+}
