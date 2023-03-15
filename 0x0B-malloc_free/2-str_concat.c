ca45486a02c8d9127d951f6d341dd9436358dfda
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-15 03:33:34 +0200

    fullpush

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,14 +11,15 @@ char *str_concat(char *s1, char *s2)
 	int l1 = 0, l2 = 0, i = 0;
 	char *s;
 
-	while (s1[l1]) 
+	while (s1 && s1[l1])
 		l1++;
-	while (s2[l2])
+	while (s2 && s2[l2])
 		l2++;
 	s = malloc((l1 + l2 + 1) * sizeof(char));
-	if (!s) 
+	if (!s)
 		return (0);
-	while (i < l1){
+	while (i < l1)
+	{
 		s[i] = s1[i];
 		i++;
 	}
