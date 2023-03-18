d807930f249bbf99245a5672fa226627f69188f8
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-18 03:53:38 +0200

    fullpush

---
+++ 0x0c-more_alloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - hi
+ * @b : yo
+ * Return: hi
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (!ptr)
+		exit(98);
+	return (ptr);
+}
