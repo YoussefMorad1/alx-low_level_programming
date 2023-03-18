ab4b6565cc273a913848b75b7329aee3e47e3bd8
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-18 04:14:24 +0200

    fullpush

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * string_nconcat - hi
+ * @s1 : yo
+ * @s2 : aa
+ * @n : sz
+ * Return: hi
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int ln1 = strlen(s1);
+	int ln2 = n;
+	char *s3;
+
+	if (strlen(s2) < ln2)
+		ln2 = strlen(s2);
+	*s3 = malloc(ln1 + ln2 + 1);
+	if (!s3)
+		return (0);
+	for (int i = 0; i < ln1 + ln2; i++)
+	{
+		if (i < ln1)
+			s3[i] = s1[i];
+		if (i > ln1)
+			s3[i] = s2[i - ln1];
+	}
+	s3[ln1 + ln2] = '\0';
+	return (s3);
+}
