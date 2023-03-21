d1ce4d4fa46a75a932aa8ed70bc643dc37ee5a5a
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-21 04:27:32 +0200

    fullpush

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,11 +11,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(1+strlen(name));
-	d->owner = malloc(1+strlen(owner));
+	d->name = malloc(strlen(name));
+	d->owner = malloc(strlen(owner));
 	strcpy(d->name, name);
 	d->age = age;
 	strcpy(d->owner, owner);
-	d->name[strlen(name)] = '\0';
-	d->owner[strlen(owner)] = '\0';
+	d->name[strlen(name)-1] = '\0';
+	d->owner[strlen(owner)-1] = '\0';
 }
