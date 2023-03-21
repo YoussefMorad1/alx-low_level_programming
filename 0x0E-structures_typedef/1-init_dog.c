11c3de712ee131b6c80fa5a73b0f5b29ab941e82
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-21 04:10:04 +0200

    fullpush

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - hi
+ * @d : hi
+ * @name : hi
+ * @age : hi
+ * @owner : hi
+ * Return: hi
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
