4152be9142e84cf0caac5965245f05b9233ebcd0
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-24 00:48:19 +0200

    fullpush

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,7 +12,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_list ls;
 
 	va_start(ls, n);
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)n; i++)
 		s += va_arg(ls, int);
 	va_end(ls);
 	return (s);
