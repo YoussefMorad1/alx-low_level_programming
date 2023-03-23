8fde4182bc1463bd82d48d2469a2638bed652f40
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-24 01:00:44 +0200

    fullpush

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,6 @@
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "variadic_functions.h"
 /**
  * print_numbers - hi
@@ -9,11 +11,12 @@
 */
 void print_numbers(const char *sep, const unsigned int n, ...)
 {
-	va_list ls = va_start(n);
+	va_list ls;
 	int i = 0;
 
 	if (!n) 
 		return;
+	va_start(ls, n);
 	for (i = 0; i < (int) n; i++)
 	{
 		if (sep && i < (int)(n - 1))
