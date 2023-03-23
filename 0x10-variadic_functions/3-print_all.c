b93d24bc7f551fb6640d2a5fdd9ff59df1e085e4
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-24 01:31:02 +0200

    fullpush

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,37 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "variadic_functions.h"
+/**
+ * print_all - hi
+ * @s : hi
+ * @... : yo
+ * Return: hi
+*/
+void print_all(const char * const s, ...)
+{
+	va_list ls;
+	const char * const p = s;
+	int ln = strlen(s), i = 0;
+
+	va_start(ls, s);
+	for (; i < ln; ++i)
+	{
+		char c = p[i];
+		char *sep = ", ";
+
+		if (i == ln - 1)
+			sep = "";
+		if (c == 'i')
+			printf("%d%s", va_arg(ls, int), sep);
+		else if (c == 'c')
+			printf("%c%s", va_arg(ls, int), sep);
+		else if (c == 'f')
+			printf("%f%s", va_arg(ls, double), sep);
+		else if (c == 's')
+			printf("%s%s", va_arg(ls, char *), sep);
+	}
+	va_end(ls);
+	printf("\n");
+}
