79b260667b11ff2fa7c89b77a9bf6ce237a7ddf8
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-24 00:57:52 +0200

    fullpush

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * print_numbers - hi
+ * @sep : hi
+ * @n : hi
+ * @... : yo
+ * Return: hi
+*/
+void print_numbers(const char *sep, const unsigned int n, ...)
+{
+	va_list ls = va_start(n);
+	int i = 0;
+
+	for (i = 0; i < (int) n; i++)
+	{
+		if (sep && i < (n - 1))
+			printf("%d%s ", va_arg(ls, int), sep);
+		else if (!sep)
+			printf("%d ", va_arg(ls, int));
+		else
+			printf("%d", va_arg(ls, int));
+	}
+	printf("\n");
+}
