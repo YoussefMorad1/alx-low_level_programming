c2c102d49b54f1e1f76b7f6c5d45470247c4db7b
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-24 01:56:03 +0200

    fullpush

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,31 +13,30 @@ void print_all(const char * const s, ...)
 {
 	va_list ls;
 	const char * const p = s;
-	int ln = strlen(s), i = 0;
+	int ln = strlen(s), i = -1;
 
 	va_start(ls, s);
-	while (i < ln)
+	while (i < ln - 1)
 	{
+		i++;
 		char c = p[i];
 		char *sep = ", ";
+		char *t;
 
 		if (i == ln - 1)
 			sep = "";
-		if (c == 'i')
+		if (c != 'i' && c != 'c' && c != 'f' && c != 's') 
+			continue;
+		else if (c == 'i')
 			printf("%d%s", va_arg(ls, int), sep);
 		else if (c == 'c')
 			printf("%c%s", va_arg(ls, int), sep);
 		else if (c == 'f')
 			printf("%f%s", va_arg(ls, double), sep);
-		else if (c == 's'){
-			char *ss = va_arg(ls, char *);
-
-			if (ss)
-				printf("%s%s", ss, sep);
-			else
-				printf("(nil)%s", sep);
-		}
-		i++;
+		else if (t = va_arg(ls, char *))
+			printf("%s%s", t, sep);
+		else
+			printf("%s%s", "(nil)", sep);
 	}
 	va_end(ls);
 	printf("\n");
