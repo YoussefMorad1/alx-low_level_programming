a7cbf5102beeb49b47dfe688657cd1ad2bd3f237
Youssef Morad <youssefmohammed747@gmail.com>
2023-03-31 05:05:10 +0200

    fullpush

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
-/** 
+/**
  * print_list - hi
  * @h : hi
  * Return: hi
@@ -10,10 +10,9 @@
 size_t print_list(const list_t *h)
 {
 	size_t x = 0;
+
 	while (h)
 	{
-		unsigned int i = 0;
-		
 		printf("[%d] ", h->len);
 		if (h->str)
 			printf("%s\n", h->str);
@@ -22,5 +21,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		x++;
 	}
-	return x;
+	return (x);
 }
