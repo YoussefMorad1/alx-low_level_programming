61c840903fd993fce4e4ab60a8d03ebc57866eb8
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-06 04:15:58 +0200

    fullpush

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,35 +3,25 @@
 #include <string.h>
 #include "lists.h"
 /**
- * add_node - hi
+ * add_nodeint - hi
  * @head : hi
- * @str : hi
+ * @n : hi
  * Return: hi
  */
-list_t *add_node(list_t **head, const char *str)
+list_t *add_nodeint(list_t **head, const int n)
 {
-	char *s;
 	list_t *h = malloc(sizeof(*h));
 
 	if (!h)
 		return (0);
-	s = strdup(str);
-	if (!s && str)
-	{
-		free(h);
-		return (0);
-	}
-	h->str = s;
+	h->n = n;
 	h->next = NULL;
-	h->len = strlen(s);
 	if (*head)
 	{
 		h->next = *head;
 		*head = h;
 	}
 	else
-	{
 		*head = h;
-	}
 	return (*head);
 }
