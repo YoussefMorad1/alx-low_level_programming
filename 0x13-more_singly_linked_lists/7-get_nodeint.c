8ff6da1104103623692820448020fceedd41c936
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-06 04:54:07 +0200

    fullpush

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - hi
+ * @h : hi
+ * @index : hi
+ * Return: hi
+ */
+listint_t *get_nodeint_at_index(listint_t *h, unsigned int index){
+	unsigned int i = 0;
+
+	while (h && i < index)
+	{
+		h =  h->next;
+		i++;
+	}
+	return h;
+}
