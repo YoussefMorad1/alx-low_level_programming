efffe504aa8e9919c20c99cc5132aec0e78bdcb0
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 20:38:38 +0200

    fullpush

--- 0x14-bit_manipulation/x.c
+++ 0x14-bit_manipulation/x.c
@@ -3,5 +3,15 @@
 int main(){
 //	printf("%d", binary_to_uint("1010"));
 	print_binary(10);
+	_putchar('\n');
+	print_binary(0);
+	_putchar('\n');
+	print_binary(1);	
+	_putchar('\n');
+	print_binary(2);
+	_putchar('\n');
+	print_binary(3);
+	_putchar('\n');
+	print_binary(4);
 	return (0);
 }
