2ba93aa72c37a9ea5d327b40aa3c5ebf4389f48e
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 21:27:38 +0200

    fullpush

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int i = 0;
 	unsigned int ct = 0;
 
-	while (i < 32ul)
+	while (i <= 32ul)
 	{
 		if ((n & (1ul << i)) != (m & (1ul << i)))
 			ct++;
