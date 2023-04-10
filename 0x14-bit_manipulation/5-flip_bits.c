112b9de5c5dbb133471c6804590aa0bb05b21cd7
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 21:27:06 +0200

    fullpush

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,14 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int i = -1;
+	unsigned long int i = 0;
 	unsigned int ct = 0;
 
-	while (++i < 32ul)
+	while (i < 32ul)
 	{
 		if ((n & (1ul << i)) != (m & (1ul << i)))
 			ct++;
+		i++;
 	}
 	return (ct);
 }
--- 0x14-bit_manipulation/x.c
+++ 0x14-bit_manipulation/x.c
@@ -1,17 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 int main(){
-//	printf("%d", binary_to_uint("1010"));
-	print_binary(10);
-	_putchar('\n');
-	print_binary(0);
-	_putchar('\n');
-	print_binary(1);	
-	_putchar('\n');
-	print_binary(2);
-	_putchar('\n');
-	print_binary(3);
-	_putchar('\n');
-	print_binary(4);
-	return (0);
+	printf("%d\n", flip_bits(0, 0));
+	printf("%d\n", flip_bits(1, 1));
+
+	printf("%d\n", flip_bits( (1ul << 31), (1ul << 31)));
 }
