330a394004e130fa2c4c447fdf2d98bd09e75978
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 21:05:29 +0200

    fullpush

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,7 +12,7 @@ void hbd(unsigned long int n, unsigned long int i)
 {
 	if ((1ul << i) > n)
 		return;
-	if (i && ((1ul << i) == 1ul))
+	if (i && ((1ul << i) == 0ul))
 		return;
 	hbd(n, i + 1);
 	_putchar('0' + ((n & (1ul << i)) ? 1ul : 0ul));
