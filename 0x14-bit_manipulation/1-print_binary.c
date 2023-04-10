8f14577db2ca8a9ed6397e3b632955d8e7737cb6
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 20:25:11 +0200

    fullpush

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * hbd - hi
+ * @n : hi
+ * @i : hi
+ * Return: hi
+ */
+void hbd(unsigned long int n, int i)
+{
+	if ((1 << i) > n) 
+		return;
+	hbd(n, i + 1);
+	printf("%d", (n & (1 << i)) ? 1 : 0);
+}
+/**
+ * binary_to_uint - hi
+ * @n : hi
+ * Return: hi
+ */
+void print_binary(unsigned long int n)
+{
+	hbd(n, 0);
+}
--- 0x14-bit_manipulation/x.c
+++ 0x14-bit_manipulation/x.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 int main(){
-	printf("%d", binary_to_uint("1010"));
+//	printf("%d", binary_to_uint("1010"));
+	print_binary(10);
 	return (0);
 }
