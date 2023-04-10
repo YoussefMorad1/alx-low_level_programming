169d25c0893ebea10c3453cdeeb84fe2d280e856
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 21:20:31 +0200

    fullpush

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * flip_bits - hi
+ * @n : hi
+ * @m : hi
+ * Return: hi
+ */
+int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int i = -1;
+
+	while (++i < 32)
+	{
+		if ( (n & (1ul << i)) != (m & (1ul << i)) )
+			ct++;
+	}
+	return (ct);
+}
