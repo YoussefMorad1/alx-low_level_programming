4600d60dcd4b0b9092b8950a3e54dee636408cb8
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-10 20:02:32 +0200

    fullpush

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * binary_to_uint - hi
+ * @b : hi
+ * Return: hi
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, ln;
+	unsigned int ans = 0;
+
+	if (!b) 
+		return (0);	
+	i = strlen(b) - 1;
+	ln = strlen(b);
+	for (; i >= 0; --i)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		ans += (b[i] - '0') * (1 << (ln - i - 1));
+	}
+	return (ans);
+}
---
+++ 0x14-bit_manipulation/x.c
@@ -0,0 +1,6 @@
+#include "main.h"
+#include <stdio.h>
+int main(){
+	printf("%d", binary_to_uint("1010"));
+	return (0);
+}
