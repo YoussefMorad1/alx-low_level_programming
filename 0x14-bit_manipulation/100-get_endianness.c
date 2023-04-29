06328809014b60d3e71ae5bda0c138d7092796f4
Youssef Morad <youssefmohammed747@gmail.com>
2023-04-29 20:41:34 +0200

    fullpush

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,11 @@
 /**
  * get_endianness - hi
- * Return : hi
+ * Return: hi
  */
 int get_endianness(void)
 {
 	int x = 1;
-	char *ptrch = (char*)&x;
+	char *ptrch = (char *)&x;
 
 	if (*ptrch)
 		return (1);
