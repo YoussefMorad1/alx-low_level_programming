2cb751a1c4599541eda951c541a2f2f5436171a3
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 02:31:18 +0200

    fullpush

---
+++ 0x1A-hash_tables/0-hash_tables_create.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+/**
+ * hash_table_create - hello
+ * @size: hi
+ * Return: hi
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_node_t **arr = malloc(sizeof(*arr) * size);
+	hash_table_t *tbl;
+
+	if (!arr)
+		return (0);
+	tbl = malloc(sizeof(hash_table_t));
+	tbl->size = size;
+	tbl->array = arr;
+	return (tbl);
+}
