74d436fbf2fb0a88ae02dfb8518902b9e95b5679
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 04:17:51 +0200

    fullpush

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int idx = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *firstNode = ht->array[idx], *ptr = firstNode;
-	hash_node_t *newNode = malloc(sizeof(newNode));
+	hash_node_t *newNode = malloc(sizeof(*newNode));
 	char *tmp;
 
 	if (!newNode || !ht)
