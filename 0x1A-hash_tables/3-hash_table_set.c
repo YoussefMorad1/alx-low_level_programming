26e87e41a94da9bfe74291f3d8a16906eef3536f
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 03:48:34 +0200

    fullpush

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,6 @@
 #include "hash_tables.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * hash_table_set - hi
  * @ht: hi
@@ -15,8 +16,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (!newNode)
 		return (0);
-	newNode->key = (char *)key;
-	newNode->value = (char *)value;
+	newNode->key = malloc(strlen(key) + 1);
+	if (newNode->key == 0)
+	{
+		free(newNode);
+		return (0);
+	}
+	newNode->value = malloc(strlen(value) + 1);
+	if (!newNode->value)
+	{
+		free(newNode->key);
+		free(newNode);
+		return (0);
+	}
+	strcpy(newNode->key, key);
+	strcpy(newNode->value, value);
 	newNode->next = 0;
 	ht->array[idx] = newNode;
 	if (firstNode)
