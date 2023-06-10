2e368e812d84c89d1244e3dc6446c2268722da3c
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 04:56:56 +0200

    fullpush

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,30 +1,31 @@
 #include "hash_tables.h"
+#include <stdio.h>
 /**
- * hash_table_delete - hi
+ * hash_table_print - hi
  * @ht: hi
  * Return: hi
- */
-void hash_table_delete(const hash_table_t *ht)
+ **/
+void hash_table_print(const hash_table_t *ht)
 {
 	int i, first = 1;
-	hash_node_t *ptr, *nxt;
+	hash_node_t *ptr;
 
 	if (!ht)
 		return;
+	printf("{");
 	for (i = 0; i < (int)ht->size; i++)
 	{
 		ptr = ht->array[i];
 
 		while (ptr)
 		{
-			nxt = ptr->next;
-			free(ptr->key);
-			free(ptr->value);
-			free(ptr);
-			ptr = nxt;
+			if (!first)
+				printf(", ");
+			else
+				first = 0;
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			ptr = ptr->next;
 		}
 	}
-	free(ht->array);
-	free(ht);
+	printf("}\n");
 }
-
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - hi
+ * @ht: hi
+ * Return: hi
+ */
+void hash_table_delete(const hash_table_t *ht)
+{
+	int i, first = 1;
+	hash_node_t *ptr, *nxt;
+
+	if (!ht)
+		return;
+	for (i = 0; i < (int)ht->size; i++)
+	{
+		ptr = ht->array[i];
+
+		while (ptr)
+		{
+			nxt = ptr->next;
+			free(ptr->key);
+			free(ptr->value);
+			free(ptr);
+			ptr = nxt;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
+
