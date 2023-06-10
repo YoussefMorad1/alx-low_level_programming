6dd9ee29c4cd6faa4bac28e8cd2d2f4ed7e767d4
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 04:51:26 +0200

    fullpush

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int idx = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node = ht->array[idx];
-
+	
+	if (!ht)
+		return (0);
+	if (!key)
+		return (0);
 	while (node)
 	{
 		if (strcmp(key, node->key) == 0)
