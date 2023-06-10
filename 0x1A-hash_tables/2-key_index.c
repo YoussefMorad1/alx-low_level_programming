a053701329b0d5794f2b46e945c3005a5a5b9b83
Youssef Morad <youssefmohammed747@gmail.com>
2023-06-10 03:05:32 +0200

    fullpush

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,6 +1,13 @@
 #include "hash_tables.h"
+/**
+ * key_index - hi
+ * @key: hi
+ * @size: size
+ * Return: hi
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_value = hash_djb2(key);
- 	return hash_value % size;	 
+
+	return (hash_value % size);
 }
